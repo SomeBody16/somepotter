014b8de2fda279540b86e3aaa339e080388abdf6
Filip <filip.nowakowski2000@outlook.com>
2023-04-21 15:01:05 +0200

    Updated particles

--- src/main/java/network/something/somemagic/util/ParticleUtil.java
+++ src/main/java/network/something/somemagic/util/ParticleUtil.java
@@ -1,5 +1,6 @@
 package network.something.somemagic.util;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
@@ -9,45 +10,18 @@ import net.minecraft.world.phys.Vec3;
 public class ParticleUtil {
 
     public static void spawn(Level level, ParticleOptions particle, Vec3 pos, int range, int particlesPerBlock) {
+        var aof = new AABB(new BlockPos(pos)).inflate(range);
+        var amount = (int) (aof.getXsize() * aof.getYsize() * aof.getZsize() * particlesPerBlock);
+        inAABBOfAmount(level, particle, aof, amount);
+
         if (level instanceof ServerLevel serverLevel) {
-            for (int r = 0; r < (range * 2); ++r) {
-                for (int c = 0; c < (range * 2); ++c) {
-                    for (int h = 0; h < (range * 2); ++h) {
-                        double x = pos.x + r - range;
-                        double y = pos.y + h - range;
-                        double z = pos.z + c - range;
-
-                        for (int l = 0; l < particlesPerBlock; ++l) {
-                            serverLevel.sendParticles(particle,
-                                    x + Math.random(), y + Math.random(), z + Math.random(),
-                                    1, 0, 0, 0, 0);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public static void inAABB(Level level, ParticleOptions particle, AABB areaOfEffect, int particlesPerBlock) {
-        if (level instanceof ServerLevel serverLevel) {
-            double minX = areaOfEffect.minX;
-            double minY = areaOfEffect.minY;
-            double minZ = areaOfEffect.minZ;
-            double maxX = areaOfEffect.maxX;
-            double maxY = areaOfEffect.maxY;
-            double maxZ = areaOfEffect.maxZ;
-
-            for (double x = minX; x <= maxX; x += 0.2) {
-                for (double y = minY; y <= maxY; y += 0.2) {
-                    for (double z = minZ; z <= maxZ; z += 0.2) {
-                        for (int l = 0; l < particlesPerBlock; ++l) {
-                            serverLevel.sendParticles(particle,
-                                    x + Math.random(), y + Math.random(), z + Math.random(),
-                                    1, 0, 0, 0, 0);
-                        }
-                    }
-                }
-            }
+            serverLevel.sendParticles(
+                    particle,
+                    pos.x, pos.y, pos.z,
+                    amount,
+                    range, range, range,
+                    0.01
+            );
         }
     }
 
@@ -60,7 +34,7 @@ public class ParticleUtil {
             for (int i = 0; i < particleAmount; i++) {
                 serverLevel.sendParticles(particle,
                         x, y, z,
-                        1, 0, 0, 0, 0);
+                        1, 3, 3, 3, 0.025);
             }
         }
     }
