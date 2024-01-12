fd4b76d36980448a35245f3be6c6f42aff469480
Filip Nowakowski <f.nowakowski2000@gmail.com>
2024-01-12 03:14:06 +0100

    Circumrota fix

--- src/main/java/network/something/somepotter/spell/circumrota/CircumrotaListener.java
+++ src/main/java/network/something/somepotter/spell/circumrota/CircumrotaListener.java
@@ -1,8 +1,8 @@
 package network.something.somepotter.spell.circumrota;
 
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.phys.EntityHitResult;
 import network.something.somepotter.event.SpellHitEvent;
 import network.something.somepotter.spell.SpellListener;
@@ -11,12 +11,12 @@ public class CircumrotaListener extends SpellListener<CircumrotaSpell> {
 
     @Override
     public void onSpellHitEntity(SpellHitEvent.Post<CircumrotaSpell> event, EntityHitResult hitResult, Entity entity) {
-        if (entity instanceof ServerPlayer player) {
-            new CircumrotaPacket().sendToClient(player);
-        } else {
-            entity.rotate(Rotation.CLOCKWISE_180);
-        }
+        var posBehind = entity.position().add(entity.getLookAngle().scale(-1));
+        entity.lookAt(EntityAnchorArgument.Anchor.EYES, posBehind);
     }
 
-
+    @Override
+    public void onSpellHitPlayer(SpellHitEvent.Post<CircumrotaSpell> event, EntityHitResult hitResult, ServerPlayer player) {
+        new CircumrotaPacket().sendToClient(player);
+    }
 }
