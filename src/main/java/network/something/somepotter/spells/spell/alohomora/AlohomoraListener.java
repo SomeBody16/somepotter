b18f7c5ecae6b61b0f93799f9bc2cb2ac71a7491
Filip Nowakowski <f.nowakowski2000@gmail.com>
2024-01-20 12:53:12 +0100

    Disable Alohomora on treasure door

--- src/main/java/network/something/somepotter/spells/spell/alohomora/AlohomoraListener.java
+++ src/main/java/network/something/somepotter/spells/spell/alohomora/AlohomoraListener.java
@@ -1,9 +1,11 @@
 package network.something.somepotter.spells.spell.alohomora;
 
+import iskallia.vault.init.ModBlocks;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraft.world.phys.BlockHitResult;
 import network.something.somepotter.event.SpellHitEvent;
+import network.something.somepotter.integration.Integrations;
 import network.something.somepotter.spells.spell.SpellListener;
 
 public class AlohomoraListener extends SpellListener<AlohomoraSpell> {
@@ -12,6 +14,12 @@ public class AlohomoraListener extends SpellListener<AlohomoraSpell> {
     public void onSpellHitBlock(SpellHitEvent.Post<AlohomoraSpell> event, BlockHitResult hitResult) {
         var blockPos = hitResult.getBlockPos();
         var blockState = event.level.getBlockState(blockPos);
+
+        if (Integrations.THE_VAULT.isLoaded()) {
+            if (blockState.is(ModBlocks.TREASURE_DOOR)) {
+                return;
+            }
+        }
 
         if (blockState.getBlock() instanceof DoorBlock doorBlock) {
             doorBlock.setOpen(null, event.level, blockState, blockPos, true);
