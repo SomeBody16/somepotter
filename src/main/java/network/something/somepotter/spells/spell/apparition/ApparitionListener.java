3e0b4684e323cc28f713f4e7c33c87a0810de592
Filip Nowakowski <f.nowakowski2000@gmail.com>
2024-01-25 18:38:10 +0100

    Add dimension check to Apparition Spell
    Dimension restriction is implemented to the Apparition Spell. This ensures that the spell can only be activated in allowed dimensions (Overworld, Nether, and End). Update also contains a minor adjustment by allowing null values for origin nodes in the Floo Network Manager.

--- src/main/java/network/something/somepotter/spells/spell/apparition/ApparitionListener.java
+++ src/main/java/network/something/somepotter/spells/spell/apparition/ApparitionListener.java
@@ -1,6 +1,7 @@
 package network.something.somepotter.spells.spell.apparition;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import network.something.somepotter.event.SpellHitEvent;
 import network.something.somepotter.floo.network.FlooNetworkManager;
@@ -8,12 +9,19 @@ import network.something.somepotter.floo.packet.OpenFlooNetworkScreenPacket;
 import network.something.somepotter.integration.Integrations;
 import network.something.somepotter.spells.spell.SpellListener;
 
+import java.util.List;
+
 public class ApparitionListener extends SpellListener<ApparitionSpell> {
 
     @Override
     public void onSpellHitPlayer(SpellHitEvent.Post<ApparitionSpell> event, EntityHitResult hitResult, ServerPlayer player) {
         // Check for research
         if (!Integrations.THE_VAULT.loadedAndHasResearch(event.level, player, "Floo Network")) {
+            return;
+        }
+
+        var allowedDimensions = List.of(Level.OVERWORLD, Level.NETHER, Level.END);
+        if (!allowedDimensions.contains(player.level.dimension())) {
             return;
         }
 
