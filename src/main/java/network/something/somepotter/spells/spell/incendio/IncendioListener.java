dea9817966373745c4bafbcf5a859f63ccee3648
Filip Nowakowski <f.nowakowski2000@gmail.com>
2024-02-21 12:28:30 +0100

    feat: Incendio spell

---
+++ src/main/java/network/something/somepotter/spells/spell/incendio/IncendioListener.java
@@ -0,0 +1,57 @@
+package network.something.somepotter.spells.spell.incendio;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import network.something.somepotter.event.SpellHitEvent;
+import network.something.somepotter.spells.spell.SpellListener;
+import network.something.somepotter.util.AbilityPowerUtil;
+
+public class IncendioListener extends SpellListener<IncendioSpell> {
+
+    @Override
+    public void onSpellHitEntity(SpellHitEvent.Post<IncendioSpell> event, EntityHitResult hitResult, Entity entity) {
+        var duration = AbilityPowerUtil.scale(event.abilityPower, 20 * 5, 20 * 60);
+        entity.setRemainingFireTicks(duration);
+    }
+
+    @Override
+    public void onSpellHitBlock(SpellHitEvent.Post<IncendioSpell> event, BlockHitResult hitResult) {
+        var blockPos = hitResult.getBlockPos();
+        var blockState = event.level.getBlockState(blockPos);
+        var direction = hitResult.getDirection();
+
+        if (blockState.getBlock() instanceof TntBlock) {
+            blockState.getBlock().onCaughtFire(blockState, event.level, blockPos, direction, event.caster);
+            event.level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), 11);
+
+        } else if (!CampfireBlock.canLight(blockState)
+                && !CandleBlock.canLight(blockState)
+                && !CandleCakeBlock.canLight(blockState)
+        ) {
+            var targetPos = blockPos.relative(direction);
+            if (BaseFireBlock.canBePlacedAt(event.level, targetPos, event.caster.getDirection())) {
+                playFireSound(event.level, targetPos);
+                var fireBlockState = BaseFireBlock.getState(event.level, targetPos);
+                event.level.setBlock(targetPos, fireBlockState, 11);
+                event.level.gameEvent(event.caster, GameEvent.BLOCK_PLACE, blockPos);
+            }
+        } else {
+            playFireSound(event.level, blockPos);
+            event.level.setBlock(blockPos, blockState.setValue(BlockStateProperties.LIT, Boolean.TRUE), 11);
+            event.level.gameEvent(event.caster, GameEvent.BLOCK_PLACE, blockPos);
+        }
+    }
+
+    protected void playFireSound(LevelAccessor pLevel, BlockPos pPos) {
+        var soundevent = SoundEvents.FLINTANDSTEEL_USE;
+        pLevel.playSound(null, pPos, soundevent, SoundSource.BLOCKS, 1.0F, 1.0F);
+    }
+}
--- src/main/java/network/something/somepotter/spells/spell/incendio/IncendioSpell.java
+++ src/main/java/network/something/somepotter/spells/spell/incendio/IncendioSpell.java
@@ -1,10 +1,71 @@
 package network.something.somepotter.spells.spell.incendio;
 
+import network.something.somepotter.init.SpellTypeInit;
+import network.something.somepotter.mechanics.gesture.SpellGesture;
+import network.something.somepotter.spells.cast.Cast;
+import network.something.somepotter.spells.cast.projectile.ProjectileCast;
+import network.something.somepotter.spells.spell.Spell;
+import network.something.somepotter.spells.spell.SpellListener;
+import network.something.somepotter.spells.spell_type.SpellType;
+import network.something.somepotter.spells.spell_type.charm.CharmType;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Sets the target on fire
  * <p>
  * Will set the target on fire for a short period of time
- * These fire will cause more damage than normal fire
  */
-public class IncendioSpell {
+public class IncendioSpell extends Spell {
+    public static final String ID = "incendio";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public @NotNull SpellListener<IncendioSpell> getListener() {
+        return new IncendioListener();
+    }
+
+    @Override
+    public @NotNull Cast getCast() {
+        return new ProjectileCast();
+    }
+
+    @Override
+    public @NotNull SpellType getType() {
+        return SpellTypeInit.get(CharmType.ID);
+    }
+
+    @Override
+    public @NotNull List<SpellGesture> getGestures() {
+        var gesture = new SpellGesture(ID);
+
+        gesture.draw.bezierCurve(25, 80, 15, 55, 50, 40, 50, 10);
+        gesture.draw.bezierCurve(50, 10, 45, 30, 80, 65, 65, 85);
+
+        return List.of(gesture);
+    }
+
+    @Override
+    public @NotNull List<SpellGesture> getMistakes() {
+        return List.of();
+    }
+
+    @Override
+    public Color getColor() {
+        var random = new Random().nextInt(100);
+        if (random < 33) {
+            return Color.RED;
+        } else if (random < 66) {
+            return Color.ORANGE;
+        } else {
+            return Color.YELLOW;
+        }
+    }
 }
